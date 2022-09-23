c473b84c64344f8dfe7668f21fef7c12f02dc09f
thelegend2742069 <pranavshirole.pvt@gmail.com>
2022-09-23 18:55:09 +0530

    init

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
 import discord
 
-token = 'MTAyMjc4MzQ1Njc1MDYxMjU0Mg.GQulS5.Dpm9xFhkcualHzGDMlxsjjkFcKWjevuWRj34JM'
+token = 'TOKEN HERE'
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 
 
+@client.event
+async def on_ready():
+    print(f"logged in as {client    .user}")
 
-
+@client.event
+async def on_message(message):
+    print(client.get_channel(id))
+    print(f"message sent by {message.author}: {message.content}")
+    if message.author == client.user:
+        return
+    else:
+        await message.channel.send("hi, this is a test message!")
 
 client.run(token)
