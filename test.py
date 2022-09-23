ac0ff8392bb612a49ead2c0f5ce2698db33395a7
thelegend2742069 <pranavshirole.pvt@gmail.com>
2022-09-23 19:30:48 +0530

    added basic command removed reply to all texts

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 
 token = 'TOKEN HERE'
 
@@ -6,21 +7,17 @@ token = 'TOKEN HERE'
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-
+client = commands.Bot(command_prefix='!', intents=intents)
 
 
 @client.event
 async def on_ready():
-    print(f"logged in as {client    .user}")
+    print(f"logged in as {client.user}")
 
-@client.event
-async def on_message(message):
-    print(client.get_channel(id))
-    print(f"message sent by {message.author}: {message.content}")
-    if message.author == client.user:
-        return
-    else:
-        await message.channel.send("hi, this is a test message!")
+
+@client.command(name='test')
+async def test(ctx):
+    await ctx.send("command test")
+
 
 client.run(token)
